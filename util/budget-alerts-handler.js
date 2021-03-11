f67d0f40efd0d866891266c55fde6ee23d01c064
Timur K <timur.khakimyanov@gmail.com>
2021-03-12 00:05:53 +0300

    Just in case, cloud function to disable billing

---
+++ util/budget-alerts-handler.js
@@ -0,0 +1,84 @@
+const {google} = require('googleapis');
+const {GoogleAuth} = require('google-auth-library');
+
+const PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT;
+const PROJECT_NAME = `projects/${PROJECT_ID}`;
+const billing = google.cloudbilling('v1').projects;
+
+exports.stopBilling = async pubsubEvent => {
+  const pubsubData = JSON.parse(
+    Buffer.from(pubsubEvent.data, 'base64').toString()
+  );
+  console.log("cost=" + pubsubData.costAmount +", budget=" + pubsubData.budgetAmount);
+  if (pubsubData.costAmount <= pubsubData.budgetAmount) {
+      console.log( `No action necessary. (Current cost: ${pubsubData.costAmount})`);
+      return
+  }
+
+  if (!PROJECT_ID) {
+    console.log('No project specified');
+    return
+  }
+
+    try {
+    _setAuthCredential();
+    const billingEnabled = await _isBillingEnabled(PROJECT_NAME);
+    if (billingEnabled) {
+      return _disableBillingForProject(PROJECT_NAME);
+    } else {
+      return 'Billing already disabled';
+    }
+  } 
+  catch (e) {
+    console.log("Exception: " + e);
+  }
+};
+
+/**
+ * @return {Promise} Credentials set globally
+ */
+const _setAuthCredential = () => {
+  const client = new GoogleAuth({
+    scopes: [
+      'https://www.googleapis.com/auth/cloud-billing',
+      'https://www.googleapis.com/auth/cloud-platform',
+    ],
+  });
+
+  // Set credential globally for all requests
+  google.options({
+    auth: client,
+  });
+};
+
+/**
+ * Determine whether billing is enabled for a project
+ * @param {string} projectName Name of project to check if billing is enabled
+ * @return {bool} Whether project has billing enabled or not
+ */
+const _isBillingEnabled = async projectName => {
+  try {
+    const res = await billing.getBillingInfo({name: projectName});
+    console.log("billingEnabled=" + res.data.billingEnabled);
+    return res.data.billingEnabled;
+  } catch (e) {
+    console.log(
+      'Unable to determine if billing is enabled on specified project, assuming billing is enabled'
+    );
+    return true;
+  }
+};
+
+/**
+ * Disable billing for a project by removing its billing account
+ * @param {string} projectName Name of project disable billing on
+ * @return {string} Text containing response from disabling billing
+ */
+const _disableBillingForProject = async projectName => {
+  console.log("Disabling billing");
+  const res = await billing.updateBillingInfo({
+    name: projectName,
+    resource: {billingAccountName: ''}, // Disable billing
+  });
+  return `Billing disabled: ${JSON.stringify(res.data)}`;
+};
